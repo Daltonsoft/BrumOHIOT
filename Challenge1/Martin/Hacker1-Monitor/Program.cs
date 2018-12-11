dba473da4e7f85c8a75b1b09aca1b36a73a614c6
Martin Kearn <martin.kearn@microsoft.com>
2018-12-11 14:23:49 +0000

    Removed monitor project

--- Challenge1/Martin/Hacker1-Monitor/Program.cs
+++ Challenge1/Martin/Hacker1-Monitor/Program.cs
@@ -1,57 +1,63 @@
-﻿using Microsoft.ServiceBus.Messaging;
+﻿using Microsoft.Azure.ServiceBus;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hacker1_Monitor
 {
     class Program
     {
-        static string connectionString = "Endpoint=sb://iothub-ns-iothubteam-1063874-205ecba883.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=iU/2kWT+XhKWHFbisUQim7dUrUpoY084vdKTSg6EoMc=";
-        static string monitoringEndpointName = "iothubteam4-martin-operationmonitoring";
-        static EventHubClient eventHubClient;
+        private static QueueClient queueClient;
+        private const string ServiceBusConnectionString = "Endpoint=sb://iothub-ns-iothubteam-1063841-9c12ea224b.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=5KI7ix7zs7T6H/KXaBhyl6J0OlTECXRAkI8qPEs6nkA=";
+        private const string QueueName = "iothubteam4-operationmonitoring";
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Monitoring. Press Enter key to exit.\n");
-
-            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, monitoringEndpointName);
-            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
-            CancellationTokenSource cts = new CancellationTokenSource();
-            var tasks = new List<Task>();
-
-            foreach (string partition in d2cPartitions)
-            {
-                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
-            }
-
-            Console.ReadLine();
-            Console.WriteLine("Exiting...");
-            cts.Cancel();
-            Task.WaitAll(tasks.ToArray());
+            MainAsync(args).GetAwaiter().GetResult();
         }
 
-        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        private static async Task MainAsync(string[] args)
         {
-            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            // Creates a ServiceBusConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            var connectionStringBuilder = new ServiceBusConnectionStringBuilder(ServiceBusConnectionString)
+            {
+                EntityPath = QueueName
+            };
+
+            // Initializes the static QueueClient variable that will be used in the ReceiveMessages method.
+            queueClient = new QueueClient(connectionStringBuilder,ReceiveMode.PeekLock);
+
+            await ReceiveMessages();
+
+            // Close the client after the ReceiveMessages method has exited.
+            await queueClient.CloseAsync();
+        }
+
+        // Receives messages from the queue in a loop
+        private static async Task ReceiveMessages()
+        {
+            Console.WriteLine("Press ctrl-c to exit receive loop.");
             while (true)
             {
-                if (ct.IsCancellationRequested)
+                try
                 {
-                    await eventHubReceiver.CloseAsync();
-                    break;
+                    // Receive the next message from the queue
+                    var message = await queueClient.
+
+                    // Write the message body to the console
+                    Console.WriteLine($"Received message: {message.GetBody<string>()}");
+
+                    // Complete the messgage so that it is not received again
+                    await message.CompleteAsync();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
                 }
 
-                EventData eventData = await eventHubReceiver.ReceiveAsync(new TimeSpan(0, 0, 10));
-
-                if (eventData != null)
-                {
-                    string data = Encoding.UTF8.GetString(eventData.GetBytes());
-                    Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
-                }
+                // Delay by 10 milliseconds so that the console can keep up
+                await Task.Delay(10);
             }
         }
+
     }
 }
